51e44f225050e99dd41b656f01eff5948184ecc9
DevOps Tinman <devops.tinman@gmail.com>
2019-11-29 07:21:52 -0600

    Initial Commit

---
+++ Gister/Gister/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gister
+//
+//  Created by Allbee, Eamon on 11/29/19.
+//  Copyright © 2019 Allbee, Eamon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
